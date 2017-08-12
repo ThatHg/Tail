0c8f2f5670e5fe4417076281da2701ac350bc11b
Hampus Gustafsson <1992hg@gmail.com>
2017-08-12 22:02:45 +0200

    Beginning of all evil.

---
+++ Tail/GraphicsWrapper.h
@@ -0,0 +1,14 @@
+#ifndef GRAPHICS_WRAPPER_H
+#define GRAPHICS_WRAPPER_H
+
+// Beginning of all evil.
+class GraphicsWrapper {
+public:
+    GraphicsWrapper();
+    ~GraphicsWrapper();
+
+private:
+
+};
+
+#endif
